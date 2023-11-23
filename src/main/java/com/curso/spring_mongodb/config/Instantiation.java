e756bb63216ec8bdb2612cbfc65236042ef8520e
Gustavo de França <gustavonota20@hotmail.com>
2023-11-23 14:20:02 -0300

    Referenciando os posts do Usuario

--- src/main/java/com/curso/spring_mongodb/config/Instantiation.java
+++ src/main/java/com/curso/spring_mongodb/config/Instantiation.java
@@ -40,5 +40,8 @@ public class Instantiation implements CommandLineRunner {
 
 
         postRepository.saveAll(Arrays.asList(post1, post2, post3));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
