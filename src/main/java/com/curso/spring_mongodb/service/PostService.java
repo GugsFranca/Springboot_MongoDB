095263fcfb00bf1dc8c6bde81212fb05ffdc1c1f
Gustavo de França <gustavonota20@hotmail.com>
2023-11-23 14:29:00 -0300

    Obtendo post por id

---
+++ src/main/java/com/curso/spring_mongodb/service/PostService.java
@@ -0,0 +1,23 @@
+package com.curso.spring_mongodb.service;
+
+import com.curso.spring_mongodb.domain.Post;
+import com.curso.spring_mongodb.repository.PostRepository;
+import com.curso.spring_mongodb.service.exception.ObjectNotFindException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> opt = repository.findById(id);
+        return opt.orElseThrow(() -> new ObjectNotFindException("Usuario não encontrado"));
+    }
+
+
+}
