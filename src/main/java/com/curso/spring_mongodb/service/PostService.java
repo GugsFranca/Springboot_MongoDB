4517e2779b7a696c707012597ff4e4664ee21efe
Gustavo de França <gustavonota20@hotmail.com>
2023-11-29 12:13:44 -0300

    fazendo consultas com varios critério

--- src/main/java/com/curso/spring_mongodb/service/PostService.java
+++ src/main/java/com/curso/spring_mongodb/service/PostService.java
@@ -6,6 +6,7 @@ import com.curso.spring_mongodb.service.exception.ObjectNotFindException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,6 +25,11 @@ public class PostService {
 //        return repository.findByTitleContainingIgnoreCase(text);
         return repository.searchTitle(text);
 
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repository.fullSearch(text, minDate, maxDate);
     }
 
 
